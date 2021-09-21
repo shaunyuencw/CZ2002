b6750c6ad93fd41d56cbfddb1abae7e2584b52d5
Shaun Yuen <91139027+shaunyuencw@users.noreply.github.com>
2021-09-21 22:40:47 +0800

    Delete Lab1 directory

--- Lab1/MyFirstProgram.java
+++
@@ -1,6 +0,0 @@
-public class MyFirstProgram{
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello! This is my first program.");
-        System.out.println("Bye Bye!");
-    }
-}
